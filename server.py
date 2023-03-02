d1f651aa64c93236db220307d078e87a98ee0b89
Parth Shah <helloparthshah@gmail.com>
2023-03-01 23:46:47 -0800

    changes
    Co-authored-by: kunpai <kunpai@ucdavis.edu>

--- server.py
+++ server.py
@@ -1,19 +1,40 @@
 from flask import Flask, request, jsonify, abort
 from flask_cors import CORS, cross_origin
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+# read secret key from .env file
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET = os.getenv('SECRET')
+print(SECRET)
+
+# domain = 'http://41a5-2601-200-c181-8670-1dd1-c0d1-f0d4-4e64.ngrok.io'
+domain = 'http://localhost:5500'
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": ["e9aa-207-183-239-54.ngrok.io   "]}})
+""" limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["1 per minute"]
+) """
+cors = CORS(app, resources={
+            r"/*": {"origins": [domain]}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.before_request
 def limit_remote_addr():
-    print(request.remote_addr)
-    print(request.headers.get('Origin'))
-    print(list(request.access_route))
-    # print domain name
-    print(request.headers.get('Host'))
-    if request.headers.get('Host') != 'e9aa-207-183-239-54.ngrok.io':
+    # check if header contains secret and if it is correct
+    if request.headers.get('Secret') != SECRET:
+        abort(403)
+    return None
+    if not request.headers.get('Origin'):
+        abort(403)
+    if domain not in request.headers.get('Origin'):
         abort(403)  # Forbidden
 
 
